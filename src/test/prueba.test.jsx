5ca57140b2ccaffc5034b742a30edf28440d9622
itexicoCarlos <carlos.vera@itexico.com>
2019-04-02 15:27:54 -0600

    Test folder ready to use

---
+++ src/test/prueba.test.jsx
@@ -0,0 +1,11 @@
+import Home from '../container/private/Home/Home.component'
+
+describe('Home Render', () => {
+  it('passing test', () => {
+    expect(Home).toBeTruthy();
+  })
+
+  it('failing test u.u', () => {
+    expect(false).toBeFalsy();
+  })
+})
